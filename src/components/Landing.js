112dc95b37ba5de8f7bbe0e7613c3da53e8924c3
Soniakeat <soniakeating@hotmail.fr>
2020-03-05 20:17:44 +0100

    in potato we trust

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,25 +1,25 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { withAuth } from "../lib/AuthProvider";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { withAuth } from '../lib/AuthProvider';
 
 class Landing extends Component {
   render() {
     return (
       <div className="landing">
-      <div>
-        <img id="logo-land" src="/images/Logo.png" alt=""/>
-      </div>
-      <div id="connect-Neighbour-land">
-        <p>Connect to your NEIGHBOUR!</p>
-      </div>
-      <div id="link-land">
-        <Link to={"signup"}>
-            <button  className="btn-land btn-edit">Signup</button>
-        </Link>
-        <Link to={"login"}>
+        <div>
+          <img id="logo-land" src="/images/Logo.png" alt="" />
+        </div>
+        <div id="connect-Neighbour-land">
+          <p>Connect to your POMME de TERRE!</p>
+        </div>
+        <div id="link-land">
+          <Link to={'signup'}>
+            <button className="btn-land btn-edit">Signup</button>
+          </Link>
+          <Link to={'login'}>
             <button className="btn-land btn-edit">Login</button>
-        </Link>
-      </div>
+          </Link>
+        </div>
       </div>
     );
   }
